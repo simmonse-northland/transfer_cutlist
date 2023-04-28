a117aade1b65fe2c15cb2332f68cc30f1738218a
Eric Simmons <simmonse@northlandbuildings.com>
2023-04-28 14:10:09 -0500

    basic file swapping

---
+++ app.py
@@ -0,0 +1,70 @@
+import tkinter as tk
+import os
+
+new_orders=r'H:\test_data\unprocessed'
+processed_orders=r'H:\test_data\processed'
+
+def get_cutlist(source_dir):
+    cutlist = []
+    for filename in os.listdir(source_dir):
+        if filename.endswith('.csv'):
+            basename = os.path.basename(filename)
+            cutlist.append(basename)
+            cutlist = sorted(cutlist, reverse=True)
+    return cutlist[:10]
+
+
+
+
+def draw_buttons(cutlist, column):
+    for row, item in enumerate(cutlist):
+        if item in new_orders_cutlist:
+            button = tk.Button(root, text=item, command=lambda item=item: button_click(item))
+        else:
+            button = tk.Button(root, text=item)
+        button.grid(row=row+1, column=column, sticky="w", padx=10, pady=5)
+
+def draw_headers():
+    new_orders_label = tk.Label(root, text="New Orders")
+    new_orders_label.grid(row=0, column=0, padx=10, pady=5)
+
+    processed_orders_label = tk.Label(root, text="Processed Orders")
+    processed_orders_label.grid(row=0, column=1, padx=10, pady=5)
+
+def button_click(item):
+    global new_orders_cutlist, processed_orders_cutlist
+    # Determine the source and destination directories based on the button clicked
+    if item in new_orders_cutlist:
+        source_dir = new_orders
+        new_orders_cutlist = get_cutlist(source_dir)
+    else: 
+        source_dir = processed_orders
+        processed_orders_cutlist = get_cutlist(source_dir)
+        cutlist = processed_orders_cutlist
+
+    # Delete the file from the source directory
+    filename = os.path.join(source_dir, item)
+    os.remove(filename)
+
+    # Redraw the buttons
+    for button in root.grid_slaves():
+        button.destroy()
+    draw_headers()
+    draw_buttons(new_orders_cutlist, 0)
+    draw_buttons(processed_orders_cutlist, 1)
+root = tk.Tk()
+
+new_orders_cutlist = get_cutlist(new_orders)
+
+processed_orders_cutlist = get_cutlist(processed_orders)
+
+new_orders_label = tk.Label(root, text="New Orders")
+new_orders_label.grid(row=0, column=0, padx=10, pady=5)
+
+processed_orders_label = tk.Label(root, text="Processed Orders")
+processed_orders_label.grid(row=0, column=1, padx=10, pady=5)
+
+draw_buttons(new_orders_cutlist, 0)
+draw_buttons(processed_orders_cutlist, 1)
+
+root.mainloop()
